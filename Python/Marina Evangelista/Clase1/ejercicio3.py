d451da32c79362a372cc926b02af712bcc4504fa
Marina Evangelista <marinaevangelista08@gmail.com>
2024-09-02 11:42:48 -0300

    Clase 1 Python-Listas y Tuplas

---
+++ Python/Marina Evangelista/Clase1/ejercicio3.py
@@ -0,0 +1,7 @@
+'''
+Ejercicio 3: Crear unrango de 3 a 10 pero pero con incremento de 2 en 2, en lugar de 1 en 1
+'''
+
+print("Rango con valores de 3 a 10, incremento 2")
+for i in range(3,11,2):
+    print(i)
