c94d772582558f46725eb232be4a7c5e75e62676
Matias Garcia <95774271+hellomaty@users.noreply.github.com>
2024-09-07 13:24:16 -0300

    Agregando Ejercicios Clase 4 /Java

---
+++ Java/Matias Garcia/leccion04/Aritmetics/src/Operaciones/Aritmrtica.java
@@ -0,0 +1,21 @@
+
+package Operaciones;
+
+/**
+ *
+ * @author Matias
+ */
+public class Aritmrtica {
+    int a ;
+    int b ;
+    
+    //Metodo
+    public  void  sumarNumero(){
+        int resultado= a + b ;
+        System.out.println("Resultado: "+resultado);
+    
+    
+    }
+    
+    
+}
