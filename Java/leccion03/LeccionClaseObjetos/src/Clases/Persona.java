d6941b0ae9bf131f373db8c38cba2e8906617404
Matias Garcia <95774271+hellomaty@users.noreply.github.com>
2024-09-23 13:18:01 -0300

    Actualizando Persona.java/clase 5

--- Java/leccion03/LeccionClaseObjetos/src/Clases/Persona.java
+++ Java/leccion03/LeccionClaseObjetos/src/Clases/Persona.java
@@ -3,7 +3,15 @@ package Clases;
 
 //Conceptos de una clase
 public class Persona {
+    //Atribuyod de la clase(caracteristas)
+    public String nombre;
+    public String apellido;
     
-    
+    //Metodos de la clase ()
+    public  void obtenerInfo(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+    }
     
 }
+
