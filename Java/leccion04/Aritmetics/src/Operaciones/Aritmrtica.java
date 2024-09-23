6e3337883ebb3446ff3862ceb1566754eface359
Matias Garcia <95774271+hellomaty@users.noreply.github.com>
2024-09-23 13:24:37 -0300

    Actualizando Aritmrtica.java Clase 5

--- Java/leccion04/Aritmetics/src/Operaciones/Aritmrtica.java
+++ Java/leccion04/Aritmetics/src/Operaciones/Aritmrtica.java
@@ -9,13 +9,33 @@ public class Aritmrtica {
     int a ;
     int b ;
     
+     //El contrucctor es un metodo especial
+    public  Aritmrtica(){//contructor vacio
+        System.out.println("Se esta ejecutano el constructor 1");
+    }
+    //Sobre carga de contructores
+    public Aritmrtica(int a,int b){//contructor 2
+            this.a=a;
+            this.b=b;
+            System.out.println("Se esta ejecutano el constructor 2");
+    }
     //Metodo
-    public  void  sumarNumero(){
+    public void sumarNumero(){
         int resultado= a + b ;
         System.out.println("Resultado: "+resultado);
     
     
     }
     
-    
+    public int sumarConRetorno(){
+        //int resultado= a + b;
+        return a + b;
+    }
+    public int sumarArgumentos(int arg1, int arg2){
+        this.a = arg1;
+        this.b = arg2;
+       return  a + b;
+       
+        
+   }
 }
