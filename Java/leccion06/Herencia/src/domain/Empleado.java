f39676c715b727c3399e09ddc57e1d9fbab9bd1a
Alex Montenegro <montenegroale38@gmail.com>
2024-10-19 08:39:53 -0300

    avance de las clase de java

--- Java/leccion06/Herencia/src/domain/Empleado.java
+++ Java/leccion06/Herencia/src/domain/Empleado.java
@@ -1,7 +1,14 @@
 package domain;
 
 public class Empleado extends Persona{
+    private int idEmpleado;
+    private double sueldo;
+    private static int contadorEmpleados; // Es para incrementar
 
-
-
+    //Constructor
+    public Empleado(String nombre, double sueldo) {
+        super(nombre);
+        this.idEmpleado = ++Empleado.contadorEmpleados;
+        this.sueldo = sueldo;
+    }
 }
