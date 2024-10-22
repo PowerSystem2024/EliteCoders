335f28c5d7aa1792ef5796289c3f553d629137db
Manolo Bassot <manolo.bassot97@gmail.com>
2024-10-22 19:33:34 -0300

    Actualizacion Portafolio

--- Python/Clase-5/Ejercicio10NoRepetir.py
+++
@@ -1,9 +0,0 @@
-#Ejercicio 10: No repetir caracteres.
-#Hacer un programa que pida una cadena por teclado , luego
-#meter los caracteres en una lista sin repetir caracteres.
-
-frase = input('Ingresa una frase: ')
-
-frase = list(set(frase))
-
-print(frase)
